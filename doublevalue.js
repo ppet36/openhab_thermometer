7a4446a72593730a1c7c7c799e69a9104fa7ec02
Pavel Petržela <ppet36@users.noreply.github.com>
2017-01-29 08:44:02 +0100

    Add files via upload

---
+++ doublevalue.js
@@ -0,0 +1,11 @@
+/**
+ * Transform script for double value. Needs to be placed in:
+ *
+ * openhab/configurations/transform
+ *
+ * directory.
+*/
+(function(i) {
+  return parseFloat(i);
+})(input)
+
